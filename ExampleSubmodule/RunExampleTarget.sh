01e9286a1b628feb9c0af93b2298d7b1410f8695
Kevin Tran <ktrain9891@gmail.com>
2017-04-14 13:56:54 -0400

    Put in a call to alloy enumeration, which was deleted in one of the old bash scripts

--- ExampleSubmodule/RunExampleTarget.sh
+++ ExampleSubmodule/RunExampleTarget.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# This section populates the database with alloys
+# source ~/fireworks/fireworks_virtualenv/bin/activate
+# maxindex=2
+# PYTHONPATH='.' luigi --module ExampleTargets  EnumerateAlloys --max-index $maxindex --writeDB --scheduler-host gilgamesh.cheme.cmu.edu  --log-level=WARNING --parallel-scheduling --worker-timeout 300  
+
 for i in {0..1000}
 do
     rm updatedDB.token
