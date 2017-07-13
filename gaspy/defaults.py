e9dee1c5503d6c4b42aa8d93d3561c4471b2d411
Kevin Tran <ktrain9891@gmail.com>
2017-07-13 15:42:40 -0700

    Fix bug to actually add constraint for OOH; reduce k

--- gaspy/defaults.py
+++ gaspy/defaults.py
@@ -174,11 +174,9 @@ def _adsorbates():
     ooh = Atoms('OOH', positions=[[0., 0., 0.],
                                   [0., 0., 1.55],
                                   [0, 0.94, 1.80]])
-    ooh.set_constraint(ase.constraints.Hookean(a1=0, a2=1, rt=1.95, k=10))  # Bind OO
+    ooh.set_constraint(ase.constraints.Hookean(a1=0, a2=1, rt=1.95, k=5))   # Bind OO
     ooh.set_constraint(ase.constraints.Hookean(a1=1, a2=2, rt=1.37, k=5))   # Bind OH
-    adsorbates['OOH'] = Atoms('OOH', positions=[[0., 0., 0.],
-                                                [0., 0., 1.55],
-                                                [0, 0.94, 1.80]])
+    adsorbates['OOH'] = ooh
 
     # All done!
     return adsorbates
