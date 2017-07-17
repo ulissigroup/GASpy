e4cafac8372ce5f1a19d23f1a8a52bbe9f625f31
Kevin Tran <ktrain9891@gmail.com>
2017-07-17 14:11:46 -0700

    Add scripts to clean the entire aux db

--- cleanup/Clean_Target/clean_fw.sh
+++ cleanup/Clean_Target/clean_fw.sh
@@ -8,7 +8,7 @@
 lpad -l /global/project/projectdirs/m2755/zu_vaspsurfaces_files/my_launchpad.yaml defuse_fws -i $1
 
 # Call the python script to purge the Aux DB
-python purgeAuxDB.py $1
+python aux_db_clean.py $1
 
 # Purge the Local DB
 ase-db /global/cscratch1/sd/zulissi/GASpy_DB/adsorption_energy_database.db fwid=$1 --delete -y
