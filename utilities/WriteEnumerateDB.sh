79f41b4f089dddb174cc7942603dc53cea7d23be
Kevin Tran <ktrain9891@gmail.com>
2017-07-19 14:15:38 -0700

    Add database-writing script and rename folder accordingly

---
+++ utilities/WriteEnumerateDB.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --time=2:00:00
+#SBATCH --partition=regular
+#SBATCH --job-name=vasp
+#SBATCH --output=enumerate-%j.out
+#SBATCH --error=enumerate-%j.error
+#SBATCH --constraint=haswell
+
+module load python
+cd /global/project/projectdirs/m2755/GASpy/utilities
+source activate /project/projectdirs/m2755/GASpy_conda/
+
+PYTHONPATH='..' luigi \
+    --module tasks EnumerateAlloys \
+    --max-index 2 \
+    --scheduler-host 128.55.144.133 \
+    --workers=1 \
+    --log-level=WARNING \
+    --parallel-scheduling \
+    --worker-timeout 300
