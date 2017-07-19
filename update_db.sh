d9d2c1614dd77a091298d016fc8108719025b1ba
Kevin Tran <ktrain9891@gmail.com>
2017-07-19 14:28:08 -0700

    Turn from head-node job to SLURM job

--- update_db.sh
+++ update_db.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --time=2:00:00
+#SBATCH --partition=regular
+#SBATCH --job-name=vasp
+#SBATCH --output=updateDB-%j.out
+#SBATCH --error=updateDB-%j.error
+#SBATCH --constraint=haswell
 
+module load python
 cd /global/project/projectdirs/m2755/GASpy
+source activate /project/projectdirs/m2755/GASpy_conda/
 
 rm /global/cscratch1/sd/zulissi/GASpy_DB/DumpToAuxDB.token
 PYTHONPATH='.' luigi \
     --module tasks UpdateAllDB \
-    --scheduler-host cori09-bond0.224 \
-    --workers=4 \
+    --max-processes 10\
+    --scheduler-host 128.55.144.133 \
+    --workers=10 \
     --log-level=WARNING \
     --parallel-scheduling \
     --worker-timeout 300
