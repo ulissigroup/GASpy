f18acab2eb45569e114a357b09c66e81f13759bc
Kevin Tran <ktrain9891@gmail.com>
2017-07-20 19:42:35 -0700

    Actually... don't use SLURM for update_db.sh

--- bash_scripts/update_db.sh
+++ bash_scripts/update_db.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=32
-#SBATCH --time=2:00:00
-#SBATCH --partition=regular
-#SBATCH --job-name=vasp
-#SBATCH --output=updateDB-%j.out
-#SBATCH --error=updateDB-%j.error
-#SBATCH --constraint=haswell
-
 module load python
 cd /global/project/projectdirs/m2755/GASpy/
 source activate /project/projectdirs/m2755/GASpy_conda/
@@ -16,8 +7,8 @@ rm /global/cscratch1/sd/zulissi/GASpy_DB/DumpToAuxDB.token
 PYTHONPATH='.' luigi \
     --module tasks UpdateAllDB \
     --max-processes 1000 \
-    --scheduler-host 128.55.144.133 \
-    --workers=10 \
+    --scheduler-host 128.55.144.139 \
+    --workers=4 \
     --log-level=WARNING \
     --parallel-scheduling \
     --worker-timeout 300
