cf422e593f475bec464d38bf93ddd6bf02b58132
Kevin Tran <ktrain9891@gmail.com>
2017-07-23 10:14:06 -0700

    Add a function to inspect an atoms object given FWID

--- gaspy/debug.py
+++ gaspy/debug.py
@@ -6,6 +6,7 @@ import sys
 import pdb
 from pprint import pprint
 from vasp.mongo import mongo_doc_atoms
+from ase.visualize import view
 from . import utils
 from . import defaults
 from . import fireworks_helper_scripts as fwhs
@@ -39,22 +40,46 @@ class AuxPull(object):
             print('DBPull fail:  no matching rows in Aux DB')
 
     def mdoc(self):
-        ''' Return the mongo document for the first matchin entry '''
+        ''' Return the mongo document for the first matching entry '''
         return self.docs[0]
     def mdocs(self):
         ''' Return the mongo documents for all matching entries '''
         return self.docs
 
     def atoms(self):
-        ''' Return the atoms object for the first matchin entry '''
+        ''' Return the atoms object for the first matching entry '''
         return mongo_doc_atoms(self.docs[0])
     def atomss(self):
         ''' Return the atoms objects for all matching entries '''
         return [mongo_doc_atoms(doc) for doc in self.docs]
 
     def fw(self):
-        ''' Return the fw object for the first matchin entry '''
+        ''' Return the fw object for the first matching entry '''
         return self.lpad.get_fw_by_id(self.docs[0]['fwid'])
     def fws(self):
         ''' Return the fw objects for all matching entries '''
         return [self.lpad.get_fw_by_id(doc['fwid']) for doc in self.docs]
+
+
+def check_atoms(fwids):
+    '''
+    Given a Fireworks ID number, this function will print out the energy and forces of
+    the system and then pull up the ase visualizer for you to look at it. If you supply
+    it with a list, then it will open all of them.
+    '''
+    # EAFP to open the list or a single value
+    try:
+        for fwid in fwids:
+            atom_puller = AuxPull(fwid=fwid)
+            atoms = atom_puller.atoms()
+            print('Looking at %s' % atoms.get_chemical_formula)
+            print('Energy:  %s eV' % atoms.get_potential_energy())
+            print('Forces:\n%s' % atoms.get_forces())
+            view(atoms)
+    except TypeError:
+        atom_puller = AuxPull(fwid=fwids)
+        atoms = atom_puller.atoms()
+        print('Looking at %s' % atoms.get_chemical_formula)
+        print('Energy:  %s eV' % atoms.get_potential_energy())
+        print('Forces:\n%s' % atoms.get_forces())
+        view(atoms)
