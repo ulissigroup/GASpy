4065a11e874f40d1970a66d8f0df5b34d79981ee
Kevin Tran <ktrain9891@gmail.com>
2017-09-24 23:25:34 -0400

    Change Luigi host from Gilgamesh to Cori

--- bash_scripts/update_db.sh
+++ bash_scripts/update_db.sh
@@ -8,7 +8,7 @@ rm /global/cscratch1/sd/zulissi/GASpy_DB/DumpToAuxDB.token
 PYTHONPATH='.' luigi \
     --module tasks UpdateAllDB \
     --max-processes 0 \
-    --scheduler-host gilgamesh.cheme.cmu.edu \
+    --scheduler-host 128.55.224.39 \
     --workers=1 \
     --log-level=WARNING \
     --parallel-scheduling \
