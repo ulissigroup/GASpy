f943243803feb54990c666b2444aadd8567c5123
Kevin Tran <ktrain9891@gmail.com>
2017-12-06 11:51:01 -0500

    Add SLURM options to shell scripts (Issue #42)

--- scripts/defuse_lost_runs.sh
+++ scripts/defuse_lost_runs.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=00:15:00
+#SBATCH --partition=regular
+#SBATCH --job-name=defuse_lost_runs
+#SBATCH --output=defuse_lost_runs-%j.out
+#SBATCH --error=defuse_lost_runs-%j.error
+#SBATCH --constraint=haswell
+
 # Go back to home directory, then go to GASpy
 cd
 cd GASpy/
---
+++ scripts/sync_images.sh
@@ -0,0 +1,2 @@
+aws s3 sync $SCRATCH/GASpy_DB/images/adsorption s3://catalyst-thumbnails/
+aws s3 sync $SCRATCH/GASpy_DB/images/catalog s3://catalyst-thumbnails/
