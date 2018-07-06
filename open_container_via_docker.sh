3adf5894aeea59e523a5dabb3e374962ef52c7e0
Kevin Tran <ktrain9891@gmail.com>
2018-07-06 09:17:28 -0400

    Add some dependencies we missed in first docker commit (#40)

--- open_container_via_docker.sh
+++ open_container_via_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# This script will use Docker to start an interactive container to run GASpy
+# This script will use Docker to start an interactive container to run GASpy.
+# Note that you need to run this script from the directory that it is in.
 
 
 # Figure out how to mount GASpy to the container
@@ -8,4 +9,4 @@ mounting_config="$gaspy_path:/home/GASpy"
 
 # Create a container from the image and then open it interactively (-it),
 # make sure it shuts down when we leave (--rm), and mount it to GASpy (-v)
-docker run -it --rm -v $mounting_config gaspy:dev
+docker run -it --rm -v $mounting_config ulissigroup/gaspy:dev
