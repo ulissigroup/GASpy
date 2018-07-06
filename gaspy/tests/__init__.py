44de7bb5d38af3509ca3a72076be1af6446881a5
Kevin Tran <ktrain9891@gmail.com>
2018-07-06 09:21:24 -0400

    Add tests for pymatgen

---
+++ gaspy/tests/__init__.py
@@ -0,0 +1,1 @@
+''' This script is intetionally blank '''
---
+++ gaspy/tests/utils_test.py
@@ -0,0 +1,24 @@
+''' Tests for the `utils` submodule '''
+
+__author__ = 'Kevin Tran'
+__email__ = 'ktran@andrew.cmu.edu'
+
+# Things we're testing
+from gaspy.utils import find_adsorption_sites
+
+# Things we need to do the tests
+import numpy as np
+import numpy.testing as npt
+from .learning_tests.pymatgen_test import _get_standard_structure
+from .regression_tests.pymatgen_regression_test import _get_sites_for_standard_structure
+
+
+def test_find_adsorption_sites():
+    '''
+    Check out `.regression_tests.pymatgen_regression_test._get_sites_for_standard_structure`
+    to see what pymatgen gives us. Our `gaspy.utils.find_adsorption_sites` simply gives us
+    the value of that object when the key is 'all'.
+    '''
+    standard_sites = _get_sites_for_standard_structure()['all']
+    sites = find_adsorption_sites(_get_standard_structure())
+    npt.assert_allclose(np.array(sites), np.array(standard_sites), rtol=1e-5, atol=-1e-7)
