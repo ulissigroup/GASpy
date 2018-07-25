00be0f64fe8f60854003b26da6aa438515689cd9
Kevin Tran <ktrain9891@gmail.com>
2018-07-25 15:53:36 -0400

    Add some quality of life things to docker image's .bashrc

---
+++ dockerfile/bashrc_additions.sh
@@ -0,0 +1,16 @@
+
+# Double-tapping tab now lists all appropriate files
+bind TAB:menu-complete
+
+# Make the terminal & ls more colorful
+export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
+export CLICOLOR=1
+export LSCOLORS=ExFxBxDxCxegedabagacad
+
+# Always use long format and show directories when using `ls`
+alias ls='ls -pl'
+
+# Make a new command to change directory and then list the files in it
+function cs () {
+    cd "$@" && ls
+    }
