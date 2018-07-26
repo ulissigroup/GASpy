5b649e2fc337417ff63572cb8a6ffeaed8c06506
Kevin Tran <ktrain9891@gmail.com>
2018-07-26 11:18:32 -0400

    Fix ls colors inside docker containers

--- dockerfile/bashrc_additions.sh
+++ dockerfile/bashrc_additions.sh
@@ -5,10 +5,9 @@ bind TAB:menu-complete
 # Make the terminal & ls more colorful
 export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
 export CLICOLOR=1
-export LSCOLORS=ExFxBxDxCxegedabagacad
 
 # Always use long format and show directories when using `ls`
-alias ls='ls -pl'
+alias ls='ls -pl --color=auto'
 
 # Make a new command to change directory and then list the files in it
 function cs () {
