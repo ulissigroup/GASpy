0bed4b85013e14e104dd3a7aed5464e23adf6e65
Kevin Tran <ktrain9891@gmail.com>
2018-08-01 17:45:44 -0400

    Remove vasp_settings_to_str (#32)

--- gaspy/utils.py
+++ gaspy/utils.py
@@ -71,30 +71,6 @@ def print_dict(d, indent=0):
         pass
 
 
-def vasp_settings_to_str(vasp_settings):
-    '''
-    This function is used in various scripts to convert a dictionary of vasp settings
-    into a format that is acceptable by ase-db.
-
-    Input:
-        vasp_settings   [dict]  Each key is a VASP setting. Each object contained therein
-                                may have a different type depending on the VASP setting.
-    Output:
-        vasp_settings   [dict]  Each key is a VASP setting. Each object contained therein
-                                is either an int, float, boolean, or string.
-    '''
-    vasp_settings = vasp_settings.copy()
-
-    # For each item in "vasp_settings"...
-    for key in vasp_settings:
-        # Find anything that's not a string, integer, float, or boolean...
-        if not isinstance(vasp_settings[key], (str, int, float, bool)):
-            # And turn it into a string
-            vasp_settings[key] = str(vasp_settings[key])
-
-    return vasp_settings
-
-
 def docs_to_pdocs(docs):
     '''
     This function turns a list of mongo documents into "parsed" mongo documents.
