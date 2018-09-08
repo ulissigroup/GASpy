ed096023e7d68f9f34691a92d364608a2be6a61a
Kevin Tran <ktrain9891@gmail.com>
2018-09-08 16:34:36 -0400

    Make GASpy-dedicated docker image (#63)

--- dockerfile/startup.sh
+++ dockerfile/startup.sh
@@ -3,11 +3,6 @@
 # creation of a Docker container. It automatically connects to your Mongo server.
 # It also opens Jupyter if you ask it to.
 
-# Optional input argument. If `1`, then start Jupyter. Otherwise continue as normal.
-jupyter="${1:-0}"
-
-
-source activate GASpy_conda
 cd
 
 # Tunnel to the Mongo server
@@ -16,11 +11,6 @@ ssh -nNT -4 \
     -L $LOCAL_AUX_HOST:$LOCAL_AUX_PORT:$AUX_HOST:$AUX_PORT \
     -L $LOCAL_AUX_HOST_READONLY:$LOCAL_AUX_PORT_READONLY:$AUX_HOST_READONLY:$AUX_PORT_READONLY \
     $MONGO_TUNNEL_USER@$MONGO_TUNNEL_HOST >> /home/logs/tunnel.log 2>&1 &
-
-# Open Jupyter
-if [ $jupyter = "jupyter" ]; then
-    jupyter notebook --ip 0.0.0.0 --no-browser --allow-root --NotebookApp.token=''
-fi
 
 # Docker will close the container when it's out of things to do.
 # This line will tell the container to do whatever else we tell it to do.
