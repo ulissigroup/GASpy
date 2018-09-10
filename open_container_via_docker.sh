35602421a60b070119829a60df10312485919e94
Kevin Tran <ktrain9891@gmail.com>
2018-09-10 18:33:45 -0400

    Residual change from giving up on running local container

--- open_container_via_docker.sh
+++ open_container_via_docker.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 # This script will use Docker to start an interactive container to run GASpy.
 # Note that you need to run this script from the directory that it is in.
-# It also assumes that you have the ssh keys necessary to access the host
-# of your Mongo server.
 
 # Optional input argument. If "jupyter", then open a container to run Jupyter.
 jupyter=${1:-0}
 
-
-# Establish out how to mount your ssh keys to the container. This is the part
-# that assumes that you have ssh keys at `~/.ssh/`
-ssh_mounting_config="$HOME/.ssh:/home/.ssh"
 
 # Establish out how to mount GASpy to the container. This is the part
 # that assumes that you are running this script inside GASpy.
