d1bb2284d9fa7ee1531e5dbbb625aa3548bb3503
Kevin Tran <ktrain9891@gmail.com>
2018-09-11 10:17:02 -0400

    Remove deprecated jupyter functionality from container opening script

--- open_container_via_docker.sh
+++ open_container_via_docker.sh
@@ -2,9 +2,6 @@
 # This script will use Docker to start an interactive container to run GASpy.
 # Note that you need to run this script from the directory that it is in.
 
-# Optional input argument. If "jupyter", then open a container to run Jupyter.
-jupyter=${1:-0}
-
 
 # Establish out how to mount GASpy to the container. This is the part
 # that assumes that you are running this script inside GASpy.
@@ -14,20 +11,9 @@ gaspy_mounting_config="$gaspy_path:/home/GASpy"
 # Create a container from the image
 #   -it     run interactively
 #   --rm    close the container when we exit
-#   -p      connect to the default port used by Jupyter
 #   -w      The container directory to start in
 #   -v      mount various things to the container
-if [ $jupyter = "jupyter" ]; then
-    docker run -it --rm -w "/home" \
-        -p 8888:8888 \
-        -v $ssh_mounting_config \
-        -v $gaspy_mounting_config \
-        ulissigroup/gaspy:v0.20 \
-        jupyter
-else
-    docker run -it --rm -w "/home" \
-        -v $ssh_mounting_config \
-        -v $gaspy_mounting_config \
-        ulissigroup/gaspy:v0.20 \
-        /bin/bash
-fi
+docker run -it --rm -w "/home" \
+    -v $gaspy_mounting_config \
+    ulissigroup/gaspy:v0.20 \
+    /bin/bash
