59546d571119fed1327176218d0b92a33a29c18f
Kevin Tran <ktrain9891@gmail.com>
2018-12-31 08:42:38 -0500

    Create MakeBulkFW task

--- gaspy/tasks/make_fireworks/core.py
+++ gaspy/tasks/make_fireworks/core.py
@@ -7,13 +7,14 @@ __emails__ = ['zulissi@andrew.cmu.edu', 'ktran@andrew.cmu.edu']
 
 import pickle
 import luigi
-from ..atoms_generators import GenerateGas
+from ..atoms_generators import GenerateGas, GenerateBulk
 from ... import defaults
 from ...mongo import make_atoms_from_doc
 from ...utils import unfreeze_dict
 from ...fireworks_helper_scripts import make_firework, submit_fwork
 
 GAS_SETTINGS = defaults.GAS_SETTINGS
+BULK_SETTINGS = defaults.BULK_SETTINGS
 
 
 class MakeGasFW(luigi.Task):
@@ -38,12 +39,49 @@ class MakeGasFW(luigi.Task):
         atoms = make_atoms_from_doc(doc)
 
         # Create, package, and submit the FireWork
+        vasp_settings = unfreeze_dict(self.vasp_settings)
         fw_name = {'gasname': self.gas_name,
-                   'vasp_settings': unfreeze_dict(self.vasp_settings),
+                   'vasp_settings': vasp_settings,
                    'calculation_type': 'gas phase optimization'}
         fwork = make_firework(atoms=atoms,
                               fw_name=fw_name,
-                              vasp_settings=unfreeze_dict(self.vasp_settings))
+                              vasp_settings=vasp_settings)
+        _ = submit_fwork(fwork=fwork, _testing=_testing)    # noqa: F841
+
+        # Pass out the firework for testing, if necessary
+        if _testing is True:
+            return fwork
+
+
+class MakeBulkFW(luigi.Task):
+    '''
+    This task will create and submit a bulk relaxation for you.
+
+    Args:
+        mpid            A string indicating the mpid of the bulk
+        vasp_settings   A dictionary containing your VASP settings
+    '''
+    mpid = luigi.Parameter()
+    vasp_settings = luigi.DictParameter(BULK_SETTINGS['vasp'])
+
+    def requires(self):
+        return GenerateBulk(mpid=self.mpid)
+
+    def run(self, _testing=False):
+        ''' Do not use `_test=True` unless you are unit testing '''
+        # Parse the input atoms object
+        with open(self.input().path, 'rb') as file_handle:
+            doc = pickle.load(file_handle)
+        atoms = make_atoms_from_doc(doc)
+
+        # Create, package, and submit the FireWork
+        vasp_settings = unfreeze_dict(self.vasp_settings)
+        fw_name = {'mpid': self.mpid,
+                   'vasp_settings': vasp_settings,
+                   'calculation_type': 'unit cell optimization'}
+        fwork = make_firework(atoms=atoms,
+                              fw_name=fw_name,
+                              vasp_settings=vasp_settings)
         _ = submit_fwork(fwork=fwork, _testing=_testing)    # noqa: F841
 
         # Pass out the firework for testing, if necessary
--- gaspy/tests/tasks_tests/make_fireworks_tests/core_test.py
+++ gaspy/tests/tasks_tests/make_fireworks_tests/core_test.py
@@ -12,12 +12,14 @@ import warnings
 warnings.filterwarnings('ignore', category=ImportWarning)
 
 # Things we're testing
-from ....tasks.make_fireworks.core import (MakeGasFW)
+from ....tasks.make_fireworks.core import (MakeGasFW,
+                                           MakeBulkFW)
 
 # Things we need to do the tests
 from ..utils import clean_up_task
 from .... import defaults
-from ....tasks.atoms_generators import GenerateGas
+from ....tasks.atoms_generators import (GenerateGas,
+                                        GenerateBulk)
 
 
 def test_MakeGasFW():
@@ -37,6 +39,28 @@ def test_MakeGasFW():
         fwork = task.run(_testing=True)
         assert fwork.name['gasname'] == 'CO'
         assert fwork.name['vasp_settings'] == defaults.GAS_SETTINGS['vasp']
+
+    finally:
+        clean_up_task(req)
+
+
+def test_MakeBulkFW():
+    task = MakeBulkFW('mp-30', defaults.BULK_SETTINGS['vasp'])
+
+    # Check that our requirment is correct
+    req = task.requires()
+    assert isinstance(req, GenerateBulk)
+    assert req.mpid == task.mpid
+
+    try:
+        # Need to make sure our requirement is run before testing our task
+        req.run()
+
+        # Manually call the `run` method with the unit testing flag to get the
+        # firework instead of actually submitting it
+        fwork = task.run(_testing=True)
+        assert fwork.name['mpid'] == 'mp-30'
+        assert fwork.name['vasp_settings'] == defaults.BULK_SETTINGS['vasp']
 
     finally:
         clean_up_task(req)
