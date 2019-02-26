19f1683bb7ed0776e7e44232a3e095fe845a4e02
Kevin Tran <ktrain9891@gmail.com>
2019-02-26 18:46:42 -0500

    Stop duplicate calculations from adding to adsorption collection multiple times

--- gaspy/tasks/db_managers/adsorption.py
+++ gaspy/tasks/db_managers/adsorption.py
@@ -10,11 +10,11 @@ __emails__ = ['zulissi@andrew.cmu.edu', 'ktran@andrew.cmu.edu']
 import traceback
 import warnings
 from datetime import datetime
-import multiprocess
+from tqdm import tqdm
 import luigi
 from ..core import get_task_output, run_task
 from ..metadata_calculators import CalculateAdsorptionEnergy
-from ...utils import print_dict
+from ...utils import print_dict, multimap
 from ...mongo import make_atoms_from_doc, make_doc_from_atoms
 from ...gasdb import get_mongo_collection
 from ...atoms_operators import fingerprint_adslab, find_max_movement
@@ -39,11 +39,10 @@ def update_adsorption_collection(n_processes=1):
 
     # Multi-thread calculations for adsorption energies
     if n_processes > 1:
-        with multiprocess.Pool(n_processes) as pool:
-            generator = pool.imap(__run_calculate_adsorption_energy_task,
-                                  missing_docs, chunksize=100)
-            calc_energy_docs = list(generator)
-    # Don't need pooling if we only want to use one thread
+        calc_energy_docs = multimap(__run_calculate_adsorption_energy_task,
+                                    missing_docs, processes=n_processes,
+                                    maxtasksperchild=10, chunksize=100,
+                                    n_calcs=len(missing_docs))
     else:
         calc_energy_docs = [__run_calculate_adsorption_energy_task(doc)
                             for doc in missing_docs]
@@ -51,10 +50,99 @@ def update_adsorption_collection(n_processes=1):
     # If a calculation fails, our helper function will return `None`. Let's
     # take those out here.
     calc_energy_docs = [doc for doc in calc_energy_docs if doc is not None]
+    # Here's another case:  sometimes we have duplicate calculations. When
+    # we go to calculate the adsorption energy of the older FWID, GASpy
+    # will pull the information of the newer FWID instead. So we'll just
+    # keep redumping newer FWID duplicates. Stop that here.
+    missing_fwids = set(doc['fwid'] for doc in missing_docs)
+    unique_calc_energy_docs = []
+    for doc in calc_energy_docs:
+        fwid = doc['fwids']['adslab']
+        if fwid in missing_fwids:
+            unique_calc_energy_docs.append(doc)
+            missing_fwids.remove(fwid)
 
     # Turn the adsorption energies into `adsorption` documents, then save them
-    adsorption_docs = [__create_adsorption_doc(doc) for doc in calc_energy_docs]
+    if n_processes > 1:
+        adsorption_docs = multimap(__create_adsorption_doc,
+                                   unique_calc_energy_docs,
+                                   processes=n_processes,
+                                   maxtasksperchild=1,
+                                   chunksize=100,
+                                   n_calcs=len(unique_calc_energy_docs))
+    else:
+        adsorption_docs = [__create_adsorption_doc(doc)
+                           for doc in unique_calc_energy_docs]
+
+    # Now write the documents
     if len(adsorption_docs) > 0:
+        with get_mongo_collection('adsorption') as collection:
+            collection.insert_many(adsorption_docs)
+        print('[%s] Created %i new entries in the adsorption collection'
+              % (datetime.now(), len(adsorption_docs)))
+
+
+def update_adsorption_collection_verbose(n_processes=1):
+    '''
+    A hacked-together copy of `update_adsorption_collection` with a bunch of
+    tqdm and print statements for debugging purposes. Useful when debugging or
+    repopulating the atoms collection. Don't use it for production.
+
+    Args:
+        n_processes     An integer indicating how many threads you want to use
+                        when running the tasks. If you do not expect many
+                        updates, stick to the default of 1, or go up to 4. If
+                        you are re-creating your collection from scratch, you
+                        may want to want to increase this argument as high as
+                        you can.
+    '''
+    # Figure out what we need to dump
+    missing_docs = _find_atoms_docs_not_in_adsorption_collection()
+
+    # Multi-thread calculations for adsorption energies
+    print('[%s] Calculating adsorption energies...' % datetime.now())
+    if n_processes > 1:
+        calc_energy_docs = multimap(__run_calculate_adsorption_energy_task,
+                                    missing_docs, processes=n_processes,
+                                    maxtasksperchild=10, chunksize=100,
+                                    n_calcs=len(missing_docs))
+    else:
+        calc_energy_docs = [__run_calculate_adsorption_energy_task(doc)
+                            for doc in tqdm(missing_docs, total=len(missing_docs))]
+
+    # If a calculation fails, our helper function will return `None`. Let's
+    # take those out here.
+    calc_energy_docs = [doc for doc in calc_energy_docs if doc is not None]
+    # Here's another case:  sometimes we have duplicate calculations. When
+    # we go to calculate the adsorption energy of the older FWID, GASpy
+    # will pull the information of the newer FWID instead. So we'll just
+    # keep redumping newer FWID duplicates. Stop that here.
+    missing_fwids = set(doc['fwid'] for doc in missing_docs)
+    unique_calc_energy_docs = []
+    for doc in calc_energy_docs:
+        fwid = doc['fwids']['adslab']
+        if fwid in missing_fwids:
+            unique_calc_energy_docs.append(doc)
+            missing_fwids.remove(fwid)
+
+    # Turn the adsorption energies into `adsorption` documents, then save them
+    print('[%s] Creating adsorption documents...' % datetime.now())
+    if n_processes > 1:
+        adsorption_docs = multimap(__create_adsorption_doc,
+                                   unique_calc_energy_docs,
+                                   processes=n_processes,
+                                   maxtasksperchild=1,
+                                   chunksize=100,
+                                   n_calcs=len(unique_calc_energy_docs))
+    else:
+        adsorption_docs = [__create_adsorption_doc(doc)
+                           for doc in tqdm(unique_calc_energy_docs,
+                                           total=len(unique_calc_energy_docs))]
+
+    # Now write the documents
+    if len(adsorption_docs) > 0:
+        print('[%s] Creating %i new entries in the adsorption collection...'
+              % (datetime.now(), len(adsorption_docs)))
         with get_mongo_collection('adsorption') as collection:
             collection.insert_many(adsorption_docs)
         print('[%s] Created %i new entries in the adsorption collection'
