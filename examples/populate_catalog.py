9a029fc60690a810be73ff803aa749a1f40e0709
Kevin Tran <ktrain9891@gmail.com>
2019-04-08 14:59:42 -0400

    Add 4 elements to the catalog population script that werely accidentally omitted

--- examples/populate_catalog.py
+++ examples/populate_catalog.py
@@ -12,9 +12,9 @@ from gaspy.tasks.db_managers.catalog import update_catalog_collection
 
 elements = ['Ag', 'Al', 'As', 'Au', 'Bi', 'Ca', 'Cd', 'Cl', 'Co', 'Cr', 'Cs',
             'Cu', 'Fe', 'Ga', 'Ge', 'H', 'Hf', 'Hg', 'In', 'Ir', 'K', 'Mn',
-            'Mo', 'N', 'Na', 'Nb', 'Os', 'P', 'Pb', 'Pd', 'Pt', 'Rh', 'Ru',
-            'S', 'Sb', 'Sc', 'Se', 'Si', 'Sn', 'Sr', 'Ta', 'Tc', 'Te', 'Ti',
-            'V', 'W', 'Y', 'Zn', 'Zr']
+            'Mo', 'N', 'Ni', 'Na', 'Nb', 'Os', 'P', 'Pb', 'Pd', 'Pt', 'Rb',
+            'Re', 'Rh', 'Ru', 'S', 'Sb', 'Sc', 'Se', 'Si', 'Sn', 'Sr', 'Ta',
+            'Tc', 'Te', 'Ti', 'Tl', 'V', 'W', 'Y', 'Zn', 'Zr']
 max_miller = 2
 
 update_catalog_collection(elements=elements, max_miller=max_miller, n_processes=32)
