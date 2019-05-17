90931d666ea612cf079684ae5b45f26e2dbc00d6
Kevin Tran <ktrain9891@gmail.com>
2019-05-17 16:37:37 -0400

    Add surface energy database updates to the pool of automatic updates

--- gaspy/tasks/db_managers/__init__.py
+++ gaspy/tasks/db_managers/__init__.py
@@ -11,8 +11,10 @@ __email__ = 'ktran@andrew.cmu.edu'
 from .catalog import update_catalog_collection
 from .atoms import update_atoms_collection
 from .adsorption import update_adsorption_collection
+from .surfaces import update_surface_energy_collection
 
 
 def update_all_collections(n_processes=1):
     update_atoms_collection(n_processes=n_processes)
     update_adsorption_collection(n_processes=n_processes)
+    udpate_surface_energy_collection(n_processes=n_processes)
