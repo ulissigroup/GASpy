d0544bf958579c6835c21d82915eddfb6ece4c95
Kevin Tran <ktrain9891@gmail.com>
2019-07-02 11:14:17 -0700

    Refactor unit testing utilities to also manage unit_testing_fireworks collection

---
+++ gaspy/tests/test_cases/fireworks/__init__.py
@@ -0,0 +1,17 @@
+'''
+Many of our unit tests expect your Fireworks server to have certain unit
+testing collections with certain contents.
+'''
+
+__author__ = 'Kevin Tran'
+__email__ = 'ktran@andrew.cmu.edu'
+
+# flake8: noqa
+from .fireworks_utils import (get_fireworks_testing_collection,
+                              create_and_populate_unit_testing_collection,
+                              create_unit_testing_collection,
+                              populate_unit_testing_collection,
+                              get_and_push_doc,
+                              get_doc,
+                              push_doc,
+                              dump_collection_to_pickle)
