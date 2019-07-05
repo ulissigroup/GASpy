6c2c0798d32253c6fd30e40f2e18c36cfff44a27
Kevin Tran <ktrain9891@gmail.com>
2019-07-05 15:21:46 -0700

    Fix typo when referencing espresso_tools

--- gaspy/fireworks_helper_scripts.py
+++ gaspy/fireworks_helper_scripts.py
@@ -198,7 +198,7 @@ def _make_qe_firework(atoms, fw_name, qe_settings):
     Args:
         atoms           `ase.Atoms` object to relax
         fw_name         Dictionary of tags/etc to use as the FireWorks name
-        qe_settings     Dictionary of settings to pass to espress_tools/Quantum
+        qe_settings     Dictionary of settings to pass to espresso_tools/Quantum
                         Espresso
     Returns:
         firework    An instance of a `fireworks.Firework` object that is set up
@@ -214,13 +214,13 @@ def _make_qe_firework(atoms, fw_name, qe_settings):
                      'git clone https://github.com/ulissigroup/espresso_tools.git')
     clone_espresso_tools = ScriptTask.from_str(clone_command)
 
-    # Tell the FireWork rocket to run the job using espress_tools
+    # Tell the FireWork rocket to run the job using espresso_tools
     atom_trajhex = encode_atoms_to_trajhex(atoms)
-    relax = PyTask(func='espress_tools.run_qe',
+    relax = PyTask(func='espresso_tools.run_qe',
                    args=[atom_trajhex, qe_settings],
                    stored_data_varname='opt_results')
 
-    # espress_tools is big. Let's remove it and then leave behind the commit
+    # espresso_tools is big. Let's remove it and then leave behind the commit
     # number we used (for traceability)
     cleaning_command = ('cd espresso_tools && '
                         'git rev-parse --verify HEAD > ../espresso_tools_version.log && '
